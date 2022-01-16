9d67d759c463be4212c38edf2aa4f45f91d5b4a7
Leonard Pepa <leopepp4@gmail.com>
2022-01-16 20:52:54 +0200

    created the grid panel

---
+++ src/GridPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+
+public class GridPanel extends JPanel {
+
+	private static final long serialVersionUID = 3728396685715564240L;
+
+	public GridPanel() {
+		this.setFocusable(true);
+	}
+}
--- src/WindowFrame.java
+++ src/WindowFrame.java
@@ -1,20 +1,30 @@
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class WindowFrame extends JFrame {
 
 	private static final long serialVersionUID = -4280772482528512323L;
-	
+
+	private JPanel container;
+
 	public static final int WIDTH = 700;
 	public static final int HEIGHT = 700;
 
 	public WindowFrame() {
+
+		container = new JPanel();
+		GridPanel gridPanel = new GridPanel();
+
+		container.add(gridPanel);
+
+		this.setContentPane(container);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		this.pack();
 		this.setVisible(true);
 		this.setLocationRelativeTo(null);
-		this.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
 	}
 
 }
